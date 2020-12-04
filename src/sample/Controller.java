078a0b21eeb54d39b66b02210a015a9ca4c8a2c3
Andreas-Ravn <andr8343@edu.easj.dk>
2020-12-04 13:51:12 +0100

    Store

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,6 +3,10 @@ package sample;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Controller {
 
     @FXML
@@ -25,6 +29,7 @@ public class Controller {
         Nr2 = Integer.parseInt(Nr2Field.getText());
         result = calculate(Nr1, Nr2, '+');
         ResultField.setText(Integer.toString(result));
+        save();
     }
 
     @FXML
@@ -33,33 +38,58 @@ public class Controller {
         Nr2 = Integer.parseInt(Nr2Field.getText());
         result = calculate(Nr1, Nr2, '-');
         ResultField.setText(Integer.toString(result));
+        save();
     }
+
     @FXML
     void multiClick() {
         Nr1 = Integer.parseInt(Nr1Field.getText());
         Nr2 = Integer.parseInt(Nr2Field.getText());
         result = calculate(Nr1, Nr2, '*');
         ResultField.setText(Integer.toString(result));
-}
+        save();
+    }
+
     @FXML
     void divideClick() {
         Nr1 = Integer.parseInt(Nr1Field.getText());
         Nr2 = Integer.parseInt(Nr2Field.getText());
         result = calculate(Nr1, Nr2, '/');
         ResultField.setText(Integer.toString(result));
+        save();
     }
-        public int calculate(int Nr1, int Nr2, char operator) {
-            switch (operator) {
-                case '+':
-                    return Nr1 + Nr2;
-                case '-':
-                    return Nr1 - Nr2;
-                case '*':
-                    return Nr1 * Nr2;
-                case '/':
-                    return Nr1 / Nr2;
-            }
-            return 0;
-        }
 
+    public int calculate(int Nr1, int Nr2, char operator) {
+        switch (operator) {
+            case '+':
+                return Nr1 + Nr2;
+            case '-':
+                return Nr1 - Nr2;
+            case '*':
+                return Nr1 * Nr2;
+            case '/':
+                return Nr1 / Nr2;
+        }
+        return 0;
     }
+    public void save() {
+        File f = new File("result.txt");
+        try {
+           if (f.createNewFile()) {
+            }
+        } catch (IOException ioException) {
+            System.out.println("Error");
+            ioException.printStackTrace();
+        }
+        try {
+            FileWriter fw = new FileWriter("result.txt");
+            fw.write(Nr1 + " " + Nr2 +  " = " + result);
+            System.out.println(Nr1);
+            System.out.println(Nr2);
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
